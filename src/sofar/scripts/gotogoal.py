f724937721b12acd321697aa5845bb21908c3adb
TakiRyo <takiryo00614@gmail.com>
2025-05-11 21:32:48 +0200

    localization and navigation finished

--- src/sofar/scripts/gotogoal.py
+++ src/sofar/scripts/gotogoal.py
@@ -34,10 +34,13 @@ class Gotogoal(Node):
 
         # State variables and thresholds
         self.obstacle_detected = False
-        self.front_clearance = 0.5  # [m]
+        self.obstacle_on_right = False
+        self.front_clearance = 0.7  # [m]
         self.side_clearance = 0.5  # [m]
-        self.desired_angle_window = 0.3 # [rad] = 17.2[deg] angle window for front scan and right scan
-        self.max_angular_speed = 1.5 # [rad/s]
+        self.desired_angle_window_front = 0.6 # [rad] = 34.4[deg] angle window for front scan 
+        self.desired_angle_window_side = 0.3 # [rad] = 17.2[deg] angle window for side scan 
+        self.max_angular_speed = 1.0 # [rad/s]
+        self.max_linear_speed = 0.3 # [m/s]
 
         # State for wall-following
         # if the robot finds a wall in front, it will turn left
@@ -46,14 +49,14 @@ class Gotogoal(Node):
   
 
         # Goals 
-        self.goals = [(1.0, 1.0), (3.0, 1.0), (5.0, 2.0), (5.0, 5.0), (2.0, 4.5)]
+        self.goals = [(1.0, 1.0), (3.0, 1.0), (5.0, 2.0), (5.0, 5.0), (-2.0, 4.5)]
         self.current_goal_index = None
 
 # -------------------------------------------------------------- #
 
     def compute_wall_following(self, out_msg):
 
-        out_msg.linear.x = 0.5
+        out_msg.linear.x = 0.0
         out_msg.angular.z = self.max_angular_speed
 
         return out_msg
@@ -63,13 +66,13 @@ class Gotogoal(Node):
     def compute_goal_following(self, out_msg, distance_to_goal, angle_diff):
 
         # Proportional control for goal-seeking
-        Kp_lin = 0.5
-        Kp_ang = 1.0
+        Kp_lin = 0.3 #1.0
+        Kp_ang = 0.8 #1.0
         linear_x = Kp_lin * distance_to_goal
         angular_z = Kp_ang * angle_diff
 
         angular_z = np.clip(angular_z, -self.max_angular_speed, self.max_angular_speed)  # max angular speed
-        linear_x = np.clip(linear_x, 0.0, 0.22)    # max linear speed
+        linear_x = np.clip(linear_x, 0.0, self.max_linear_speed)    # max linear speed
 
         out_msg.linear.x = linear_x
         out_msg.angular.z = angular_z
@@ -136,11 +139,11 @@ class Gotogoal(Node):
 
 
         # Navgation mode switching
-        if not self.following_wall and self.obstacle_detected:
+        if not self.following_wall and (self.obstacle_detected or self.obstacle_on_right) :
             self.following_wall = True
             self.get_logger().info("Obstacle detected: switching to wall-following.")
 
-        elif self.following_wall and not self.obstacle_detected:
+        elif self.following_wall and not self.obstacle_detected and not self.obstacle_on_right:
             self.following_wall = False
             self.get_logger().info("Path to goal clear: resuming goal-seeking.")
 
@@ -177,11 +180,12 @@ class Gotogoal(Node):
         # angle_increment = 0.0174533 it is positive so it means lidar rotates counter-clockwise
         idx_front = round((0.0 - scan_msg.angle_min)/scan_msg.angle_increment)   # = 0
         idx_right = round((-np.pi/2 - scan_msg.angle_min)/scan_msg.angle_increment) # = -90 
-        window = round(self.desired_angle_window/scan_msg.angle_increment) # = 17
+        window_front = round(self.desired_angle_window_front/scan_msg.angle_increment) # = 34
+        window_right = round(self.desired_angle_window_side/scan_msg.angle_increment) # = 17
         
-        front_indices_1 = slice(idx_front, idx_front+window+1) # [0, 17]
-        front_indices_2 = slice(idx_front-window+num_rays, idx_front+num_rays) # [-17, 0]+360 = [343, 359]
-        right_indices = slice(idx_right-window+num_rays, idx_right+window+num_rays+1) # [-107, -74] + 360 = [253, 288] 
+        front_indices_1 = slice(idx_front, idx_front+window_front+1) # [0, 34]
+        front_indices_2 = slice(idx_front-window_front+num_rays, idx_front+num_rays) # [-34, 0]+360 = [343, 359]
+        right_indices = slice(idx_right-window_right+num_rays, idx_right+window_right+num_rays+1) # [-107, -74] + 360 = [253, 288] 
 
         # Compute stats
         front_dist_1 = np.min(ranges[front_indices_1])  # uses MIN to detect most dangerous obstacle
